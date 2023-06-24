df2adad57378acef275e61d48de5918e7c40dc03
wadeen <meister.vvdo@gmail.com>
2023-06-24 16:36:48 +0900

    🌱 First commit

---
+++ routes/index.tsx
@@ -0,0 +1,14 @@
+import { Head } from "$fresh/runtime.ts";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>React Link Card</title>
+      </Head>
+      <div>
+        <h1>Welcome to React Link Card</h1>
+      </div>
+    </>
+  );
+}
